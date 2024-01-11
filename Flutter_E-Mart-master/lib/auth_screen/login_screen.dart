0ae549e8b2444501f91912213386d6992fbc8e72
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-01-11 09:28:52 +0545

    inital commit

---
+++ Flutter_E-Mart-master/lib/auth_screen/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../widgets_common/bg_widget.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return bgWidget(
+      child: Scaffold(
+    body: Container(),
+      ));
+  }
+}
