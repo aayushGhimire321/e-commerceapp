4ff75648bd809a5dcc7114d602183195a40d3a93
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-01-17 19:03:57 +0545

    working on login screen

--- Flutter_E-Mart-master/lib/auth_screen/login_screen.dart
+++ Flutter_E-Mart-master/lib/auth_screen/login_screen.dart
@@ -1,15 +1,36 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/widgets_common/applogo_widget.dart';
+import 'package:emart_app/widgets_common/custom_textfield.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets_common/bg_widget.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return bgWidget(
       child: Scaffold(
-    body: Container(),
+    body: Center(
+      child: Column(
+        children:[
+          (context.screenHeight * 0.1).heightBox,
+          applogoWidget(),
+          10.heightBox,
+          "Log in to $appname".text.fontFamily(bold).white.size(18).make(),
+          10.heightBox,
+
+          Column(
+            children: [
+              customTextField(hint: emailHint,title: email),
+              customTextField(hint: passwordHint, title: password),
+              TextButton(onPressed: (){}, child: child)
+            ],
+          ).box.white.rounded.padding(EdgeInsets.all(16)).width(context.screenWidth - 70).make(),
+        ],
+      ),
+    ),
       ));
   }
 }
--- Flutter_E-Mart-master/lib/consts/strings.dart
+++ Flutter_E-Mart-master/lib/consts/strings.dart
@@ -1,3 +1,10 @@
 const appname = "eMart";
 const appversion = "Version 1.0.0";
 const credits = "@Aayush";
+const email = "Email";
+const emailHint="admin@admin.com";
+const password = "Password";
+const passwordHint ="********";
+const forgetPass = "Forget Password";
+const login = "Log in";
+const signup= "Sign up";
