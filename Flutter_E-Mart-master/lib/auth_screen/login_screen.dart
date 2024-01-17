fd3085e654e66f993fe91889b40b9ce37792cb2f
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-01-17 21:13:57 +0545

    works ongoing

--- Flutter_E-Mart-master/lib/auth_screen/login_screen.dart
+++ Flutter_E-Mart-master/lib/auth_screen/login_screen.dart
@@ -7,35 +7,61 @@ import 'package:flutter/material.dart';
 import '../widgets_common/bg_widget.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}): super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return bgWidget(
-      child: Scaffold(
-    body: Center(
-      child: Column(
-        children:[
-          (context.screenHeight * 0.1).heightBox,
-          applogoWidget(),
-          10.heightBox,
-          "Log in to $appname".text.fontFamily(bold).white.size(18).make(),
-          10.heightBox,
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            (context.screenHeight * 0.1).heightBox,
+            applogoWidget(),
+            10.heightBox,
+            "Log in to $appname".text.fontFamily(bold).white.size(18).make(),
+            10.heightBox,
+            Column(
+              children: [
+                customTextField(hint: emailHint, title: email),
+                customTextField(hint: passwordHint, title: password),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {}, child: forgetPass.text.make())),
+                5.heightBox,
+                ourButton(
+                        color: redColor,
+                        title: login,
+                        textColor: whiteColor,
+                        onPress: () {})
+                    .box
+                    .width(context.screenWidth - 50)
+                    .make(),
 
-          Column(
-            children: [
-              customTextField(hint: emailHint,title: email),
-              customTextField(hint: passwordHint, title: password),
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(onPressed: (){}, child: forgetPass.text.make())),
-              5.heightBox,
-              ourButton().box.width(context.screenWidth - 50).make(),
-            ],
-          ).box.white.rounded.padding(EdgeInsets.all(16)).width(context.screenWidth - 70).make(),
-        ],
+                5.heightBox,
+                createNewAccount.text.color(fontGrey).make(),
+                5.heightBox,
+                ourButton(
+                    color: golden,
+                    title: signup,
+                    textColor: redColor,
+                    onPress: () {})
+                    .box
+                    .width(context.screenWidth - 50)
+                    .make(),
+
+              ],
+            )
+                .box
+                .white
+                .rounded
+                .padding(const EdgeInsets.all(16))
+                .width(context.screenWidth - 70)
+                .make(),
+          ],
+        ),
       ),
-    ),
-      ));
+    ));
   }
 }
