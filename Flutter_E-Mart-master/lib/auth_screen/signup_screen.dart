b9a2ad876c6dc7f76524741ce68bc826630667e6
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-01-17 22:06:25 +0545

    progress

---
+++ Flutter_E-Mart-master/lib/auth_screen/signup_screen.dart
@@ -0,0 +1,100 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:flutter/material.dart';
+
+import '../consts/lists.dart';
+import '../widgets_common/applogo_widget.dart';
+import '../widgets_common/bg_widget.dart';
+import '../widgets_common/custom_textfield.dart';
+import '../widgets_common/our_button.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return bgWidget(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: [
+            (context.screenHeight * 0.1).heightBox,
+            applogoWidget(),
+            10.heightBox,
+            "Join the $appname".text.fontFamily(bold).white.size(18).make(),
+            15.heightBox,
+            Column(
+              children: [
+                customTextField(hint: nameHint, title: name),
+                customTextField(hint: emailHint, title: email),
+                customTextField(hint: passwordHint, title: password),
+                customTextField(hint: passwordHint, title: retypePassword),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {}, child: forgetPass.text.make())),
+                Row(
+                  children: [
+                    Checkbox(
+                      checkColor: redColor,
+                      value: false,
+                      onChanged: (newValue) {},
+                    ),
+                    10.widthBox,
+                    Expanded(
+                      child: RichText(
+                          text: const TextSpan(
+                        children: [
+                          TextSpan(
+                              text: "I agree to the ",
+                              style: TextStyle(
+                                fontFamily: bold,
+                                color: fontGrey,
+                              )),
+                          TextSpan(
+                              text: termAndCond,
+                              style: TextStyle(
+                                fontFamily: bold,
+                                color: redColor,
+                              )),
+                          TextSpan(
+                              text: " & ",
+                              style: TextStyle(
+                                fontFamily: bold,
+                                color: fontGrey,
+                              )),
+                          TextSpan(
+                              text: privacyPolicy,
+                              style: TextStyle(
+                                fontFamily: bold,
+                                color: redColor,
+                              ))
+                        ],
+                      )),
+                    ),
+                  ],
+                ),
+                5.heightBox,
+                ourButton(
+                    color: redColor,
+                    title: signup,
+                    textColor: whiteColor,
+                    onPress: () {})
+                    .box
+                    .width(context.screenWidth - 50)
+                    .make(),
+              ],
+            )
+                .box
+                .white
+                .rounded
+                .padding(const EdgeInsets.all(16))
+                .width(context.screenWidth - 70)
+                .shadowSm
+                .make(),
+          ],
+        ),
+      ),
+    ));
+  }
+}
