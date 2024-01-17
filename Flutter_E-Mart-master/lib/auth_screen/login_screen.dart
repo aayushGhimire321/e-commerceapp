c4c5e07adaaa0fe8d1d6a12d2c828229e0fa8077
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-01-17 19:26:25 +0545

    work on progress

--- Flutter_E-Mart-master/lib/auth_screen/login_screen.dart
+++ Flutter_E-Mart-master/lib/auth_screen/login_screen.dart
@@ -30,7 +30,7 @@ class LoginScreen extends StatelessWidget {
                   alignment: Alignment.centerRight,
                   child: TextButton(onPressed: (){}, child: forgetPass.text.make())),
               5.heightBox,
-              ourButton(),
+              ourButton().box.make(),
             ],
           ).box.white.rounded.padding(EdgeInsets.all(16)).width(context.screenWidth - 70).make(),
         ],
