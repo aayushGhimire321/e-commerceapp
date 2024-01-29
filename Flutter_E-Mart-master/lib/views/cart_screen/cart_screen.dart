efbc18c18f859e09441d9af8cbc0ba4c70c2a4e3
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-01-29 20:53:54 +0545

    Cart screen work done

--- Flutter_E-Mart-master/lib/views/cart_screen/cart_screen.dart
+++ Flutter_E-Mart-master/lib/views/cart_screen/cart_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class CartScreen extends StatelessWidget {
+  const CartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.purple,
+      color: Colors.purple
     );
   }
 }
