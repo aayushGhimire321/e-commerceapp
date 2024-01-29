2dd866e50c623426d9ad68a2c16f87d023023f2f
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-01-29 17:46:39 +0545

    changes on going

--- Flutter_E-Mart-master/lib/views/auth_screen/login_screen.dart
+++ Flutter_E-Mart-master/lib/views/auth_screen/login_screen.dart
@@ -2,6 +2,7 @@
 import 'package:emart_app/consts/consts.dart';
 import 'package:emart_app/consts/lists.dart';
 import 'package:emart_app/views/auth_screen/signup_screen.dart';
+import 'package:emart_app/views/home_screen/home_screen.dart';
 import 'package:emart_app/widgets_common/applogo_widget.dart';
 import 'package:emart_app/widgets_common/custom_textfield.dart';
 import 'package:emart_app/widgets_common/our_button.dart';
@@ -42,7 +43,9 @@ class LoginScreen extends StatelessWidget {
                         color: redColor,
                         title: login,
                         textColor: whiteColor,
-                        onPress: () {})
+                        onPress: () {
+                          Get.to(()=>HomeScreen());
+                        })
                     .box
                     .width(context.screenWidth - 50)
                     .make(),
