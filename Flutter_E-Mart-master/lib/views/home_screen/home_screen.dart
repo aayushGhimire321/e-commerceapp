be15fb1b4f42d5a665f4bdfa1e47e69d7b12b89c
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-02-04 15:10:41 +0545

    work on progress

--- Flutter_E-Mart-master/lib/views/home_screen/home_screen.dart
+++ Flutter_E-Mart-master/lib/views/home_screen/home_screen.dart
@@ -23,6 +23,7 @@ class HomeScreen extends StatelessWidget {
               color: lightGrey,
               child: TextFormField(
                 decoration: const InputDecoration(
+                  border: InputBorder.none,
                   suffixIcon: Icon(Icons.search),
                   filled: true,
                   fillColor: whiteColor,
@@ -31,36 +32,103 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-
-            //swipers brands
-            VxSwiper.builder(
-                aspectRatio: 16 / 9,
-                autoPlay: true,
-                height: 150,
-                enlargeCenterPage: true,
-                itemCount: slidersList.length,
-                itemBuilder: (context, index) {
-                  return Image.asset(
-                    slidersList[index],
-                    fit: BoxFit.fill,
-                  )
-                      .box
-                      .rounded
-                      .clip(Clip.antiAlias)
-                      .margin(const EdgeInsets.symmetric(horizontal: 8))
-                      .make();
-                }),
             10.heightBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                  2,
-                  (index) => homeButtons(
-                      height: context.screenHeight * 0.15,
-                      width: context.screenWidth / 2.5,
-                      icon: index == 0 ? icTodaysDeal : icFlashDeal,
-                      title: index == 0 ? todayDeal : flashsale,
-                  )),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    //swiper brands
+                    VxSwiper.builder(
+                        aspectRatio: 16 / 9,
+                        autoPlay: true,
+                        height: 150,
+                        enlargeCenterPage: true,
+                        itemCount: slidersList.length,
+                        itemBuilder: (context, index) {
+                          return Image.asset(
+                            slidersList[index],
+                            fit: BoxFit.fill,
+                          )
+                              .box
+                              .rounded
+                              .clip(Clip.antiAlias)
+                              .margin(const EdgeInsets.symmetric(horizontal: 8))
+                              .make();
+                        }),
+                    10.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                          2,
+                          (index) => homeButtons(
+                                height: context.screenHeight * 0.15,
+                                width: context.screenWidth / 2.5,
+                                icon: index == 0 ? icTodaysDeal : icFlashDeal,
+                                title: index == 0 ? todayDeal : flashsale,
+                              )),
+                    ),
+                    //2nd swiper
+                    10.heightBox,
+                    //swipers brands
+                    VxSwiper.builder(
+                        aspectRatio: 16 / 9,
+                        autoPlay: true,
+                        height: 150,
+                        enlargeCenterPage: true,
+                        itemCount: secondSlidersList.length,
+                        itemBuilder: (context, index) {
+                          return Image.asset(
+                            secondSlidersList[index],
+                            fit: BoxFit.fill,
+                          )
+                              .box
+                              .rounded
+                              .clip(Clip.antiAlias)
+                              .margin(const EdgeInsets.symmetric(horizontal: 8))
+                              .make();
+                        }),
+
+                    //category buttons
+                    10.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                          3,
+                          (index) => homeButtons(
+                                height: context.screenHeight * 0.15,
+                                width: context.screenWidth / 3.5,
+                                icon: index == 0
+                                    ? icTopCategories
+                                    : index == 1
+                                        ? icBrands
+                                        : icTopSeller,
+                                title: index == 0
+                                    ? topCategories
+                                    : index == 1
+                                        ? brand
+                                        : topSellers,
+                              )),
+                    ),
+                    //featured categories
+                    20.heightBox,
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: featuredCategories.text
+                            .color(darkFontGrey)
+                            .size(18)
+                            .fontFamily(semibold)
+                            .make()),
+                    20.heightBox,
+                    Row(
+                      children: List.generate(3,(index)=>Column(
+                        children: [],
+                      )
+                      ).toList(),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
