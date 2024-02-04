7532914b44cd74ec13c83a0b97049c15d2c49eeb
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-02-04 08:29:02 +0545

    work on progress

--- Flutter_E-Mart-master/lib/consts/strings.dart
+++ Flutter_E-Mart-master/lib/consts/strings.dart
@@ -2,18 +2,32 @@ const appname = "eMart";
 const appversion = "Version 1.0.0";
 const credits = "@Aayush";
 const email = "Email";
-const emailHint="admin@admin.com";
+const emailHint = "admin@admin.com";
 const password = "Password";
-const passwordHint ="********";
-const retypePassword="Retype Password";
-const name="Name";
-const nameHint="Aayush";
+const passwordHint = "********";
+const retypePassword = "Retype Password";
+const name = "Name";
+const nameHint = "Aayush";
 const forgetPass = "Forget Password";
 const login = "Log in";
-const signup= "Sign up";
-const createNewAccount="or, create a new account";
+const signup = "Sign up";
+const createNewAccount = "or, create a new account";
 const loginWith = "Log in with";
 const privacyPolicy = "Privacy Policy";
-const termAndCond="Terms and Conditions";
-const alreadyHaveAccount="Already have an Account? ";
-const home ="Home", categories="Categories", cart="Cart", account="Account";
+const termAndCond = "Terms and Conditions";
+const alreadyHaveAccount = "Already have an Account? ";
+
+//home strings
+const home = "Home",
+    categories = "Categories",
+    cart = "Cart",
+    account = "Account";
+
+//home screen strings
+const searchanything = "Search anything....",
+    todayDeal = "Today's Deal",
+    flashsale = "Flash Sale",
+    topSellers = "Top Sellers",
+    brand = "Brand",
+    topCategories = "Top Categories",
+    featuredCategories = "Featured Categories";
--- Flutter_E-Mart-master/lib/views/home_screen/home_screen.dart
+++ Flutter_E-Mart-master/lib/views/home_screen/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/widgets_common/home_buttons.dart';
 import 'package:flutter/material.dart';
 
-import '../../consts/colors.dart';
+import '../../consts/lists.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}): super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +14,57 @@ class HomeScreen extends StatelessWidget {
       color: lightGrey,
       width: context.screenWidth,
       height: context.screenHeight,
-      child: SafeArea(child: Column(
-        children: [
-          Container(
-            color: lightGrey,
-            child: TextFormField(),
-          ),
-        ],
-      ),),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 60,
+              color: lightGrey,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  suffixIcon: Icon(Icons.search),
+                  filled: true,
+                  fillColor: whiteColor,
+                  hintText: searchanything,
+                  hintStyle: TextStyle(color: textfieldGrey),
+                ),
+              ),
+            ),
+
+            //swipers brands
+            VxSwiper.builder(
+                aspectRatio: 16 / 9,
+                autoPlay: true,
+                height: 150,
+                enlargeCenterPage: true,
+                itemCount: slidersList.length,
+                itemBuilder: (context, index) {
+                  return Image.asset(
+                    slidersList[index],
+                    fit: BoxFit.fill,
+                  )
+                      .box
+                      .rounded
+                      .clip(Clip.antiAlias)
+                      .margin(const EdgeInsets.symmetric(horizontal: 8))
+                      .make();
+                }),
+            10.heightBox,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: List.generate(
+                  2,
+                  (index) => homeButtons(
+                      height: context.screenHeight * 0.15,
+                      width: context.screenWidth / 2.5,
+                      icon: index == 0 ? icTodaysDeal : icFlashDeal,
+                      title: index == 0 ? todayDeal : flashsale,
+                  )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
